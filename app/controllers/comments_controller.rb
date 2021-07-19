8ee4fa459e63f0e48a3f51f76ef2f29efcd2c90c
Milan <milanvpage@gmail.com>
2021-07-19 14:41:39 -0700

    commit tree 89d6d2e68375cd5c5153545e9e4d443cc5f3b32f

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,14 @@ class CommentsController < ApplicationController
 
   def create
     comment = Comment.create(comment_params)
+
+    if params[:comment][:user_id] != ""
+      @new_user = User.find_by(id: comment_params[:user_id])
+    else
+      @new_user = User.create(comment_params[:user_attributes])
+    end
+    comment.user = @new_user
+    comment.save
     redirect_to comment.post
   end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,14 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(categories_attributes)
+    categories_attributes.vaules.each do |categories_attributes|
+      if !categories_attributes[:name].empty?
+        category = Category.find_or_create_by(categories_attributes)
+        self.categories << category
+      end
+    end
+  end
+
 
 end
